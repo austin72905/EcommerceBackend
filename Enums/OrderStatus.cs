b5c3edc9987cb63a8bccff094d839ce14c94a06f
AUSTIN72905\Linpo <Linponggood@gmail.com>
2024-09-10 18:15:40 +0800

    新增 enums、api 交互格式

---
+++ Enums/OrderStatus.cs
@@ -0,0 +1,36 @@
+﻿namespace EcommerceBackend.Enums
+{
+    public enum OrderStatus
+    {
+        /// <summary>
+        /// 等待付款
+        /// </summary>
+        WAIT_PAY=0,
+
+        /// <summary>
+        /// 等待出貨
+        /// </summary>
+        WAIT_SHIPPMENT=1,
+
+        /// <summary>
+        /// 等待取貨
+        /// </summary>
+        WAIT_PICKUP=2,
+
+        /// <summary>
+        /// 已完成
+        /// </summary>
+        COMPLETE=3,
+
+        /// <summary>
+        /// 已取消
+        /// </summary>
+        CANCELED =4,
+
+        /// <summary>
+        /// 退貨/款
+        /// </summary>
+        REFUND =5
+
+    }
+}
---
+++ Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceBackend.Models
+{
+    public class ApiResponse
+    {
+        public int Code { get; set; }
+
+        public string? Message { get; set; }
+
+        public object? Data { get; set; }
+    }
+}
